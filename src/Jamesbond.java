ce1e87c6d7b0cf33ed299a72e7d522d1794dadb9
ilabstaff <ilabstaff@172.16.116.68>
2020-01-22 19:27:40 -0800

    hripzi

---
+++ src/Jamesbond.java
@@ -0,0 +1,12 @@
+
+public class Jamesbond {
+	
+int findCode(Vault v) {
+	for (int i = 1; i < 1000001; i++) {
+	if(	v.tryCode(i)==true) {
+		return i;
+	}
+	
+} return-1;
+}                                                
+}
---
+++ src/Vault.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Vault {
+int secretCode = new Random().nextInt(1000000)+ 1;
+boolean  tryCode (int code){
+	if (code == secretCode) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}}
